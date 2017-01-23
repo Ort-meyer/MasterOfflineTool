19a33845a9c5a05d7571505a3d93b2a1a703d4ac
Azoooka <putte_dt@hotmail.se>
2017-01-23 15:21:34 +0100

    Added test data file, and method to generate test data

--- Master Offline Tool/Master Offline Tool/main.cpp
+++ Master Offline Tool/Master Offline Tool/main.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
 using namespace std;
 
+#include <FANN\header\floatfann.h>
+#include <FANN\header\fann_cpp.h>
+
+#include <fstream>
+
+void CreatetestFile()
+{
+	int min = 0;
+	int max = 100;
+	int numTestCases = 50;
+	ofstream file;
+	file.open("PlusTest.data");
+	/* 
+	Print first line of data:
+	First is number of test cases.
+	next is number of inputs
+	finally is number of outputs*/
+	file << numTestCases << " " << "2" << " " << "1" << endl;
+	// Loop through our test cases
+	for (int i = 0; i < numTestCases; i++)
+	{
+		// Create random numbers
+		int x = rand() % (max - min + 1) + min;
+		int y = rand() % (max - min + 1) + min;
+		if (x == y)
+			cout << "fuck";
+		// Print the numbers as inputs and their sum as output
+		file << x << " " << y << endl << x+y << endl;
+	}
+	file.close();
+
+}
+
 int main()
 {
 	cout << "Hello, world!" << endl;
+	CreatetestFile();
+	const float learning_rate = 0.7f;
+	const unsigned int num_layers = 3;
+	const unsigned int num_input = 2;
+	const unsigned int num_hidden = 3;
+	const unsigned int num_output = 1;
+	const float desired_error = 0.001f;
+	const unsigned int max_iterations = 300000;
+	const unsigned int iterations_between_reports = 1000;
+
+
+
 	int pause;
 	cin >> pause;
 }
