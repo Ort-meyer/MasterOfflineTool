4c933b1dfca63269c9d632f4b0013972bd25808a
Konrad Öhman <konrad_o_92@hotmail.com>
2017-02-23 15:02:42 +0100

    Start of lost visualisation

---
+++ Heatmap builder/Heatmap builder/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Heatmap_builder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenerateHeatmap = new System.Windows.Forms.Button();
+            this.Preview = new System.Windows.Forms.PictureBox();
+            this.InFolder = new System.Windows.Forms.TextBox();
+            this.OutFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Preview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GenerateHeatmap
+            // 
+            this.GenerateHeatmap.Location = new System.Drawing.Point(12, 467);
+            this.GenerateHeatmap.Name = "GenerateHeatmap";
+            this.GenerateHeatmap.Size = new System.Drawing.Size(320, 56);
+            this.GenerateHeatmap.TabIndex = 0;
+            this.GenerateHeatmap.Text = "Generate Heatmap";
+            this.GenerateHeatmap.UseVisualStyleBackColor = true;
+            this.GenerateHeatmap.Click += new System.EventHandler(this.GenerateHeatmap_Click);
+            // 
+            // Preview
+            // 
+            this.Preview.Location = new System.Drawing.Point(366, 12);
+            this.Preview.Name = "Preview";
+            this.Preview.Size = new System.Drawing.Size(374, 261);
+            this.Preview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Preview.TabIndex = 1;
+            this.Preview.TabStop = false;
+            // 
+            // InFolder
+            // 
+            this.InFolder.Location = new System.Drawing.Point(197, 12);
+            this.InFolder.Name = "InFolder";
+            this.InFolder.Size = new System.Drawing.Size(163, 22);
+            this.InFolder.TabIndex = 3;
+            this.InFolder.Text = "LostData";
+            // 
+            // OutFolder
+            // 
+            this.OutFolder.Location = new System.Drawing.Point(197, 40);
+            this.OutFolder.Name = "OutFolder";
+            this.OutFolder.Size = new System.Drawing.Size(163, 22);
+            this.OutFolder.TabIndex = 4;
+            this.OutFolder.Text = "HeatMaps";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Folder containing data files";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Folder to save heatmaps";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 535);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutFolder);
+            this.Controls.Add(this.InFolder);
+            this.Controls.Add(this.Preview);
+            this.Controls.Add(this.GenerateHeatmap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Preview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateHeatmap;
+        private System.Windows.Forms.PictureBox Preview;
+        private System.Windows.Forms.TextBox InFolder;
+        private System.Windows.Forms.TextBox OutFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Heatmap builder/Heatmap builder/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Heatmap_builder
+{
+    public partial class Form1 : Form
+    {
+        Bitmap b;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Form functions
+        private void GenerateHeatmap_Click(object sender, EventArgs e)
+        {
+            string[] allHeatmapFiles = AllHeatmapFilesInDirectory(InFolder.Text);
+            for (int i = 0; i < allHeatmapFiles.Length; i++)
+            {
+                int minX, minY, maxX, maxY;
+                List<int[]> allLostPositions = ReadRawData(out minX, out minY, out maxX, out maxY, allHeatmapFiles[i]);
+
+                b = new Bitmap(maxX - minX + 1, maxY - minY + 1);
+                Graphics g = Graphics.FromImage(b);
+                
+                g.Clear(Color.FromArgb(1, Color.Red));
+                for (int currentPosition = 0; currentPosition < allLostPositions.Count; currentPosition++)
+                {
+                    Color pixelColor = b.GetPixel(allLostPositions[currentPosition][0] - minX, allLostPositions[currentPosition][1] - minY);
+                    b.SetPixel(allLostPositions[currentPosition][0] - minX, allLostPositions[currentPosition][1] - minY
+                        , Color.FromArgb(pixelColor.A + 254/allHeatmapFiles.Length, pixelColor));
+                }
+                      
+                
+                Preview.Image = b;
+
+                string folderPath = Path.GetFullPath(OutFolder.Text);
+                b.Save(folderPath + "ShouldNotBeNamedThis" + ".png", ImageFormat.Png);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+         
+        // Helper Functions
+        private List<int[]> ReadRawData(out int minX, out int minY, out int maxX, out int maxY, string fileName)
+        {
+            List<int[]> returnList = new List<int[]>();
+            string[] allLines = File.ReadAllLines(fileName);
+
+            // Loop unroll to get start max min value
+            int[] position = new int[2];
+            string thisLine = allLines[0];
+            string[] numbers = thisLine.Split(' ');
+            position[0] = int.Parse(numbers[0]);
+            position[1] = int.Parse(numbers[1]);
+            returnList.Add(position);
+            minX = position[0];
+            minY = position[1];
+            maxX = minX;
+            maxY = minY;
+
+            for (int i = 1; i < allLines.Length; i++)
+            {
+                position = new int[2];
+                thisLine = allLines[i];
+                numbers = thisLine.Split(' ');
+                position[0] = int.Parse(numbers[0]); //X
+                position[1] = int.Parse(numbers[2]); //Z
+                returnList.Add(position);
+                minX = Math.Min(minX, position[0]);
+                minY = Math.Min(minY, position[1]);
+                maxX = Math.Max(maxX, position[0]);
+                maxY = Math.Max(maxY, position[1]);
+            }
+            return returnList;
+        }
+
+        private string[] AllHeatmapFilesInDirectory(string directory)
+        {
+            string[] allFiles = Directory.GetFiles(directory, "*.heatmap");
+            return allFiles;
+        }
+    }
+}
