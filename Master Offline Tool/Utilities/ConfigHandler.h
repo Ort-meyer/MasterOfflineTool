19661d5a811a4abedf1fff26b69756e5173e2db8
john bergsten <putte_dt@hotmail.se>
2017-02-24 13:41:15 +0100

    added some comments in confighandler header

--- Master Offline Tool/Utilities/ConfigHandler.h
+++ Master Offline Tool/Utilities/ConfigHandler.h
@@ -4,7 +4,10 @@ class ConfigHandler
 public:
     static ConfigHandler* Get();
 
+    // Filtering attributes
+    // Number of entries we want to avrage across
     int m_entriesToAvrage;
+    // Number of entries we want merged on the same row
     int m_entriesToMerge;
 
 private:
