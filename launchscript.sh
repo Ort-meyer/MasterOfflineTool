85b697886a1a2e7b5d2d192280c2205dc5a60681
john bergsten <putte_dt@hotmail.se>
2017-03-28 12:06:49 +0200

    Added launch parameter script

---
+++ launchscript.sh
@@ -0,0 +1,25 @@
+cd Master\ Offline\ Tool/Debug
+
+
+declare -a combinationsToUse=("Key" "Pos" "Rot" "KeyPos" "KeyRot" "PosRot" "KeyPosRot")
+
+declare -a hiddenLayers=(
+	10 10 10 0 0
+	20 10 10 0 0
+	5 30 20 10 0
+)
+
+declare -a hiddenLayerCount=(
+	2
+	3
+	4
+)
+
+for combination in "${combinationsToUse[@]}"
+do
+	layerLength=${#hiddenLayerCount[@]}
+	for ((i = 0; i<${layerLength}; i++))
+	do
+		./master\ Offline\ Tool.exe $combination ${hiddenLayerCount[i]} ${hiddenLayers[i*5]} ${hiddenLayers[i*5+1]} ${hiddenLayers[i*5+2]} ${hiddenLayers[i*5+3]} ${hiddenLayers[i*5+4]}
+	done
+done
