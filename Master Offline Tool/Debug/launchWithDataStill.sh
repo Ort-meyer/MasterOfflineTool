7d25523db541dc6e1c903ad58a6731852ec4c9af
john bergsten <putte_dt@hotmail.se>
2017-03-28 13:48:19 +0200

    work in progress on launch scripts

--- Master Offline Tool/Debug/launchWithDataStill.sh
+++ Master Offline Tool/Debug/launchWithDataStill.sh
@@ -1,14 +1,28 @@
 #cd Master\ Offline\ Tool/Debug
 
-declare -a entriesToAvrage=(10 60 240)
-declare -a entriesToMerge=(3 5 20 40)
+declare -a entriesToAvrage=(
+	60 
+	20 
+	240
+)
+declare -a entriesToMerge=(
+	6
+	3 
+	20 
+	40
+)
 
 for avrage in "${entriesToAvrage[@]}"
 do
 	for merge in "${entriesToAvrage[@]}"
 	do
+		rm -R SavednetSettings/.
 		echo "running still with setting: " $avrage $merge
-		./DataStill64.exe $avrage $merge
-		sh ./launchscript.sh
+		#./DataStill64.exe $avrage $merge
+		#sh ./launchscript.sh
+		cd ..
+		mkdir -p "SavedNetSettingsDifferentStills/$avrage $merge"
+		cp -R SavedNetSettings/. "SavedNetSettingsDifferentStills/$avrage $merge"
+		cd Debug
 	done
 done
--- Master Offline Tool/Debug/launchscript.sh
+++ Master Offline Tool/Debug/launchscript.sh
@@ -1,20 +1,32 @@
 #cd Master\ Offline\ Tool/Debug
 
 
-declare -a combinationsToUse=("Key" "Pos" "Rot" "KeyPos" "KeyRot" "PosRot" "KeyPosRot")
+#declare -a combinationsToUse=("Key" "Pos" "Rot" "KeyPos" "KeyRot" "PosRot" "KeyPosRot")
+
+declare -a combinationsToUse=("PosRot" "KeyPos" "KeyRot" "Pos" "Rot" "Key" "KeyPosRot")
 
 declare -a hiddenLayers=(
-	10 5 0 0 0
+	40 0 0 0 0 #one layer
+	20 0 0 0 0
+	10 0 0 0 0
+	10 5 0 0 0 #cone down layers
 	20 10 5 0 0
 	40 30 20 10 0
 	100 70 50 30 10
+	20 60 20 0 0 #out then in layers
+	20 40 80 20 0
 )
 
 declare -a hiddenLayerCount=(
+	1
+	1
+	1
 	2
 	3
 	4
 	5
+	3
+	4
 )
 
 for combination in "${combinationsToUse[@]}"
