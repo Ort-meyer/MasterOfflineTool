49f9f24a2e1f0968c3ede7641df724f00a27e632
john bergsten <putte_dt@hotmail.se>
2017-03-29 10:00:44 +0200

    loads of net settings results

--- Master Offline Tool/Debug/launchscript.sh
+++ Master Offline Tool/Debug/launchscript.sh
@@ -6,27 +6,27 @@
 declare -a combinationsToUse=("PosRot" "KeyPos" "KeyRot" "Pos" "Rot" "Key" "KeyPosRot")
 
 declare -a hiddenLayers=(
-	40 0 0 0 0 #one layer
+	#40 0 0 0 0 #one layer
 	20 0 0 0 0
-	10 0 0 0 0
+	#10 0 0 0 0
 	10 5 0 0 0 #cone down layers
-	20 10 5 0 0
+	#20 10 5 0 0
 	40 30 20 10 0
-	100 70 50 30 10
+	#100 70 50 30 10
 	20 60 20 0 0 #out then in layers
-	20 40 80 20 0
+	#20 40 80 20 0
 )
 
 declare -a hiddenLayerCount=(
+	#1
 	1
-	1
-	1
+	#1
 	2
-	3
+	#3
 	4
-	5
+	#5
 	3
-	4
+	#4
 )
 
 for combination in "${combinationsToUse[@]}"
