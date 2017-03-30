05c163bcab6948ccafd5daa6c0a5f91f96ad0f0d
john bergsten <putte_dt@hotmail.se>
2017-03-30 10:50:44 +0200

    a prepare script

--- gnuplotpreparescript.sh
+++ gnuplotpreparescript.sh
@@ -1,1 +1,88 @@
 
+baseLayers="2  10 5" # Don't forget, TWO SPACES between first two numbers
+baseConfig="PosRot"
+baseStill="60 5"
+
+# Combination variation first
+#target="Master Offline Tool/SavedNetSettingsDifferentStills/*/"
+# Enter directory where we have all different stills
+cd "Master Offline Tool/SavedNetSettingsDifferentStills/"
+#echo $baseStill
+# Enter still
+cd "${baseStill}"
+# Iterate over every combination
+for d2 in */; do
+	#echo "${d2}"
+	cd "${d2}"
+	# Enter folder where net results are
+	cd "1110/"
+	# Iterate over every network
+	for f in *; do
+		# Only grab the right network
+		if [[ ${f} == *"${baseLayers}"* ]]; then
+			#echo "${f}"
+			cp "${f}" "../../../../../Gnuplot result graph data/Combination varied" #something like this?
+			fi
+	done
+	# All files read, jump out to 1110 and then to where all combinations are
+	cd "../../"
+done
+cd "../"
+pwd
+#echo $destination "===going into===" $target
+
+
+# Layer variation second
+cd "${baseStill}"
+cd "${baseConfig}"
+cd "1110/"
+for f in *; do
+	cp "${f}" "../../../../../Gnuplot result graph data/Layers varied"
+done
+# Return to pwd
+cd "../../.."
+pwd
+
+
+# Vary still last
+for d in */; do
+	echo "${d}"
+	cd "${baseConfig}"
+	cd "1110/"
+	for f in *; do
+		# Only grab the right network
+		if [[ ${f} == *"${baseLayers}"* ]]; then
+			#echo "${f}"
+			cp "${f}" "../../../../../Gnuplot result graph data/Datastill varied" #something like this?
+			fi
+	done
+done
+
+exit 1
+
+
+# Sortof general implementation that just gets all networks
+#target="Master Offline Tool/SavedNetSettingsDifferentStills/*/"
+# Enter directory where we have all different stills
+cd "Master Offline Tool/SavedNetSettingsDifferentStills/"
+for d in */; do # Should only be one folder here
+	echo $d
+	# Enter still
+    cd "${d}"
+    # Iterate over every combination
+    for d2 in */; do
+    	echo "${d2}"
+    	cd "${d2}"
+    	# Enter folder where net results are
+    	cd "1110/"
+    	# Iterate over every network
+    	for f in *; do
+    		echo "${f}" #For now we only print the network. Make sure copy is done!
+    		cp "${f}" "../../../../../Gnuplot result graph data/Combination varied" #something like this?
+    	done
+    	# All files read, jump out to 1110 and then to where all combinations are
+    	cd "../../"
+    done
+    cd "../"
+done
+#echo $destination "===going into===" $target
