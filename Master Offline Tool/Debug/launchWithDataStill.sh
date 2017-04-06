21747849a4aa4bc67c31f0522a195a9e4c51e43d
Konrad Öhman <konrad_o_92@hotmail.com>
2017-04-06 13:40:00 +0200

    Changes to run from baseline

--- Master Offline Tool/Debug/launchWithDataStill.sh
+++ Master Offline Tool/Debug/launchWithDataStill.sh
@@ -18,14 +18,14 @@ do
 		# Launch the actual tool
 	    sh ./launchscript.sh "$avrage $merge"
 		## Start working on saving results for latest still
-		cd ..
-		# Create new directory for this still
-		mkdir -p "SavedNetSettingsDifferentStills/$avrage $merge"
-		# Move results
-		mv SavedNetSettings/* "SavedNetSettingsDifferentStills/$avrage $merge"
-		# Copy a certain file back so git doesn't bug out
-		mv "SavedNetSettingsDifferentStills/$avrage $merge/allActivationFunctions.bigrun" SavedNetSettings
-		# Reset folder for next run
-		cd Debug
+		#cd ..
+		## Create new directory for this still
+		#mkdir -p "SavedNetSettingsDifferentStills/$avrage $merge"
+		## Move results
+		#mv SavedNetSettings/* "SavedNetSettingsDifferentStills/$avrage $merge"
+		## Copy a certain file back so git doesn't bug out
+		#mv "SavedNetSettingsDifferentStills/$avrage $merge/allActivationFunctions.bigrun" SavedNetSettings
+		## Reset folder for next run
+		#cd Debug
 	done
 done
