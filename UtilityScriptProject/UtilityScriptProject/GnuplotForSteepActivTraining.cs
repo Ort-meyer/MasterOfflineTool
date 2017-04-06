aeb92f2249d71bd294b8aa5b7090b8123c2162ef
john bergsten <putte_dt@hotmail.se>
2017-04-06 16:36:03 +0200

    work on progress on script to get steepness and shti right

--- UtilityScriptProject/UtilityScriptProject/GnuplotForSteepActivTraining.cs
+++ UtilityScriptProject/UtilityScriptProject/GnuplotForSteepActivTraining.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ namespace UtilityScriptProject
      activation functions and training algorithm*/
     class GnuplotForSteepActivTraining
     {
+        string m_baseTargetPath;
         public GnuplotForSteepActivTraining()
         {
             string baseHiddenSteepness = "0.6";
@@ -20,7 +22,7 @@ namespace UtilityScriptProject
             string baseOutputActiv = "10";
             string baseTraining = "2";
             string sourcePath = @"..\..\..\..\Master Offline Tool\SavedNetSettingsSteepActivTrain\";
-            string baseTargetPath = @"..\..\..\..\Gnuplot result graph data";
+            m_baseTargetPath = @"..\..\..\..\Gnuplot result graph data";
 
             // Get a big list of all files
             List<string> allFiles = new List<string>();
@@ -44,12 +46,12 @@ namespace UtilityScriptProject
                 if (
                      //info[1] == baseHiddenActiv &&
                      info[2] == baseOutputActiv &&
-                     info[8+int.Parse(info[3])-1] == baseTraining &&
+                     info[8 + int.Parse(info[3]) - 1] == baseTraining &&
                      info[10 + int.Parse(info[3]) - 1] == baseHiddenSteepness &&
                      info[11 + int.Parse(info[3]) - 1] == baseOutputSteepness)
                 {
-                    Directory.CreateDirectory(baseTargetPath + "\\Hidden activ varied\\");
-                    File.Copy(file, baseTargetPath + "\\Hidden activ varied\\" + Path.GetFileName(file), true);
+                    Directory.CreateDirectory(m_baseTargetPath + "\\Hidden activ varied\\");
+                    File.Copy(file, m_baseTargetPath + "\\Hidden activ varied\\" + Path.GetFileName(file), true);
                 }
 
                 // Output activ
@@ -60,8 +62,8 @@ namespace UtilityScriptProject
                     info[10 + int.Parse(info[3]) - 1] == baseHiddenSteepness &&
                     info[11 + int.Parse(info[3]) - 1] == baseOutputSteepness)
                 {
-                    Directory.CreateDirectory(baseTargetPath + "\\Output activ varied\\");
-                    File.Copy(file, baseTargetPath + "\\Output activ varied\\" + Path.GetFileName(file), true);
+                    Directory.CreateDirectory(m_baseTargetPath + "\\Output activ varied\\");
+                    File.Copy(file, m_baseTargetPath + "\\Output activ varied\\" + Path.GetFileName(file), true);
                 }
 
                 // Training
@@ -72,8 +74,8 @@ namespace UtilityScriptProject
                     info[10 + int.Parse(info[3]) - 1] == baseHiddenSteepness &&
                     info[11 + int.Parse(info[3]) - 1] == baseOutputSteepness)
                 {
-                    Directory.CreateDirectory(baseTargetPath + "\\Training varied\\");
-                    File.Copy(file, baseTargetPath + "\\Training varied\\" + Path.GetFileName(file), true);
+                    Directory.CreateDirectory(m_baseTargetPath + "\\Training varied\\");
+                    File.Copy(file, m_baseTargetPath + "\\Training varied\\" + Path.GetFileName(file), true);
                 }
 
                 // Hidden steep
@@ -84,8 +86,8 @@ namespace UtilityScriptProject
                     //info[10] == baseHiddenSteepness &&
                     info[11 + int.Parse(info[3]) - 1] == baseOutputSteepness)
                 {
-                    Directory.CreateDirectory(baseTargetPath + "\\Hidden steep varied\\");
-                    File.Copy(file, baseTargetPath + "\\Hidden steep varied\\" + Path.GetFileName(file), true);
+                    Directory.CreateDirectory(m_baseTargetPath + "\\Hidden steep varied\\");
+                    File.Copy(file, m_baseTargetPath + "\\Hidden steep varied\\" + Path.GetFileName(file), true);
                 }
 
                 // Output steep
@@ -94,15 +96,95 @@ namespace UtilityScriptProject
                     info[2] == baseOutputActiv &&
                     info[8 + int.Parse(info[3]) - 1] == baseTraining &&
                     info[10 + int.Parse(info[3]) - 1] == baseHiddenSteepness)
-                    //info[11] == baseOutputSteepness)
+                //info[11] == baseOutputSteepness)
                 {
-                    Directory.CreateDirectory(baseTargetPath + "\\Output steep varied\\");
-                    File.Copy(file, baseTargetPath + "\\Output steep varied\\" + Path.GetFileName(file), true);
+                    Directory.CreateDirectory(m_baseTargetPath + "\\Output steep varied\\");
+                    File.Copy(file, m_baseTargetPath + "\\Output steep varied\\" + Path.GetFileName(file), true);
                 }
 
             }
 
+            // Files are moved, now calculate stuff and put into gnuplot-ready file
 
+            BarPlot("Hidden activ", 1);
+            BarPlot("Output activ", 2);
+            BarPlot("Training", 8);
+
+
+
+        }
+
+        private void PrepareLinePlotData()
+        {
+
+        }
+
+        private void BarPlot(string p_type, int p_infoIndex)
+        {
+            List<FileEntry> entriesList = new List<FileEntry>();
+            string path = Path.GetFullPath(m_baseTargetPath);
+            string newPath = path + "\\" + p_type + " varied";
+            
+            string[] files = Directory.GetFiles(newPath);
+            foreach (string filePath in files)
+            {
+                // We don't want to try and read our dummy file
+                if (Path.GetExtension(filePath) != ".txt")
+                {
+                    string[] lineInfo = Path.GetFileName(filePath).Split();
+                    string thisLabel = lineInfo[p_infoIndex];
+                    // Get values
+                    float meanCorrect = 0, meanWrong = 0, correctStandardDeviation = 0, wrongStandardDeviation = 0;
+
+                    GnuplotUtilities.GetValues(filePath, ref meanCorrect, ref meanWrong, ref correctStandardDeviation, ref wrongStandardDeviation);
+                    string thisText;
+                    if (p_infoIndex != 8)
+                        thisText = GetActivationFunctionName(lineInfo[p_infoIndex]);
+                    else
+                    {
+                        thisText = GetTrainingName(lineInfo[8 + int.Parse(lineInfo[3]) - 1]);
+                    }
+                    FileEntry thisEntry = new FileEntry(thisText, meanCorrect, meanWrong, correctStandardDeviation, wrongStandardDeviation);
+                    entriesList.Add(thisEntry);
+                }
+            }
+            ///////////////////BIGASS BUG HERE FOR SOME REASON//////////////
+            GnuplotUtilities.SaveValuesToFile(path, entriesList);
+            string gnuplotPath = path + "\\gnuplot bar script error bars - generic.plt";
+            // -e "filename='default.data'" - e "foo='bar"'
+            string args = "-e \"fileName='" + p_type + ".txt'\" -e \"outputFileName='" + p_type + ".svg'\"";
+            Process.Start(gnuplotPath, args);
+
+
+        }
+
+        // Simple method that gets the activation function of a certain index
+        private string GetActivationFunctionName(string p_index)
+        {
+            Dictionary<string, string> activationFunctionmap = new Dictionary<string, string>();
+            activationFunctionmap["11"] = "Elliot symmetric";
+            activationFunctionmap["5"] = "Sigmoid symmetric";
+            activationFunctionmap["15"] = "Cos symmetric";
+            activationFunctionmap["14"] = "Sin symmetric";
+            activationFunctionmap["8"] = "Gaussian symmetric";
+            activationFunctionmap["13"] = "Linear piece symmetric";
+            activationFunctionmap["10"] = "Elliot";
+            activationFunctionmap["3"] = "Sigmoid";
+            activationFunctionmap["7"] = "Gaussian";
+            activationFunctionmap["12"] = "Linear piece";
+
+            return activationFunctionmap[p_index];
+        }
+
+        private string GetTrainingName(string p_index)
+        {
+            Dictionary<string, string> trainingMap = new Dictionary<string, string>();
+            trainingMap["1"] = "Incremental";
+            trainingMap["2"] = "RProp";
+            trainingMap["3"] = "QuickProp";
+            trainingMap["4"] = "SarProp";
+
+            return trainingMap[p_index];
         }
     }
 }
